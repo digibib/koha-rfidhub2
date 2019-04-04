d42010bd0dbfd5bfff382b13d1e0c41024a1067a
Benjamin Rokseth <benjamin.rokseth@deichman.no>
2019-04-04 11:41:04 +0200

    Millisecond not MilliSecond

--- main.go
+++ main.go
@@ -97,7 +97,7 @@ func main() {
 		const msg = `{"sender":%q,"branch":%q,"client_IP":%q,"barcode":%q,"sip_message_type":%q}`
 		go func() {
 			client := http.Client{
-				Timeout: time.Duration(100 * time.MilliSecond),
+				Timeout: time.Duration(100 * time.Millisecond),
 			}
 			for m := range logToRFID {
 				var b bytes.Buffer
